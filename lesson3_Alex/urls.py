cdcd008cbca17829a4ecb63c7ab684327f093034
Александр <apamic1995@gmail.com>
2020-02-24 20:02:06 +0200

    change

--- lesson3_Alex/urls.py
+++ lesson3_Alex/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     re_path(r'base/$', views.base),
     re_path(r'home/$', views.home),
-    re_path(r'my_game/(<number>[\d]{0,4})?/$', views.my_game)
+    re_path(r'my_game/(?P<number>[\d]{1,4})/$', views.my_game)
 ]
