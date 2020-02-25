f06612270a32f9df81145d8e8863e23a12f148f7
marjan <dima.marjan@gmail.com>
2020-02-25 18:00:14 +0200

    upload

--- marjan/my_project/my_page_2/urls.py
+++ marjan/my_project/my_page_2/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from my_page_2 import views
 
 urlpatterns = [
-    path('my_page_2/', views.first_page),
+    # path('my_page_2/', views.first_page),
+    path('my_page_2/', views.content_art),
 ]
