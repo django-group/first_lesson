4eb2d79508c9956ecc216208418b8c620ae32549
Александр <apamic1995@gmail.com>
2020-02-27 14:55:00 +0200

    lesson4 fin

--- lesson4_Alex/views.py
+++ lesson4_Alex/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
 
 # Create your views here.
+
+from .models import Author
+
+def authtors(request):
+    try:
+        authors = Author.objects.all()
+    except Author.DoesNotExist:
+        raise Http404("Author does not exist")
+    return render(request, 'authors.html', {"authors": authors})
