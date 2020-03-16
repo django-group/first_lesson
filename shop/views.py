4b15f15bff78b3d5bcc8035320b1ecdf04c8c7ab
dimamarjan <dima.marjan@gmail.com>
2020-03-16 18:49:19 +0200

    upload

--- shop/views.py
+++ shop/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 
 def foo(requests):
-    return HttpResponse('<br><br><p><a href="http://127.0.0.1:8000/admin">Админка</a></p>')
+    return render(requests, 'shop/index.html')
