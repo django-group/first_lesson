0ee7d98644e7ef6e2b6e88e6071efff9caf0e51f
dimamarjan <dima.marjan@gmail.com>
2020-03-19 12:54:51 +0200

    upload

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from shop import models
 
 
 def foo(requests):
-    return render(requests, 'shop/index.html')
+    context = {"admin": "http://127.0.0.1:8000/admin"}
+    prod = models.Product.getall()
+    print(type(prod))
+    return render(requests, 'shop/index.html', context, prod)
